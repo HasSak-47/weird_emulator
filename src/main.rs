13bbb4e53eb7a9fa91c58eeccd0c7ebad39e1f28
lilith machine <lilith.alanis.255@gmail.com>
2023-04-17 13:22:09 -0600

    readded processor

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
 mod ram;
+mod stack;
 mod processor;
 
+use processor::Pu8;
+
 fn main() {
-    /*
-    let mut machine = Processor::new();
+    let mut machine = Pu8::new();
 
     // how the asm should kinda be
-    let memory = [
-        // mov val 0x11 to reg 0x00
-        0x50, 0x20, 0x00, 0x11,
-        // mov val 0x11 to reg 0x01
-        0x50, 0x20, 0x01, 0x11,
+    let instructions = [
+        // mov val 0xf0 to reg 0x00
+        0x50, 0x20, 0x00, 0xf0,
         // cmp adr at reg 0x00 and val 0x00
         0x40, 0x23, 0x00, 0x00,
-        // je to 0x10
-        0x22, 0x10,
         // add from val 0x01 to reg 0x00
         0x00, 0x20, 0x00, 0x01,
-        // jmp to 0x04
-        0x20, 0x04,
+        // jne to 0x04
+        0x21, 0x04,
+
+        0x01, 0x20, 0x00, 0xf0,
     
         0xf0, // End
+    ];
+
+    //starts at 0xf0
+    let memory = [
         // fake string
         0x01, 0x02, 0x03, 0x4, 0x00,
     ];
 
-    for val in 0..memory.len() {
-        machine.ram[val] = memory[val];
+    for i in 0..instructions.len() {
+        machine.ram[i] = instructions[i];
     }
 
-    while machine.ram[machine.ram_ptr] != 0xf0{
-        println!("{:?}", machine);
-        let mut buffer = String::new();
-        std::io::stdin().read_line(&mut buffer).unwrap();
+    for i in 0..memory.len() {
+        let p = i + 0xf0;
+        println!("{} {} {}", p, 0xf0, 0x0f);
+        machine.ram[p] = memory[i];
+    }
+
+    while machine.ram[machine.inst_ptr] != 0xf0{
+        // println!("{:?}", machine);
+        // let mut buffer = String::new();
+        // std::io::stdin().read_line(&mut buffer).unwrap();
 
         machine.step();
     }
-    */
+    println!("{:?}", machine);
 }
